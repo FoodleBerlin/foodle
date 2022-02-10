f3fcd15b4ea0da81a87dc84330292f2d11b2ded0
Alex <alex.alber.aa@gmail.com>
2022-02-10 14:08:48 +0100

    feat: Add landingInfo component

---
+++ components/Layout/LandingInfo.tsx
@@ -0,0 +1,24 @@
+import { NextComponentType, NextPage } from "next";
+import Link from "next/link";
+import Image from "next/image";
+import React from "react";
+import styles from "../../styles/pages/Home.module.scss";
+import Tab from "./Tab";
+import Search from "./Search";
+import { BurgerMenu } from "./BurgerMenu";
+
+interface LandingInfoProps {
+  leftText: string;
+  rightText: string;
+  containerStyle: string;
+}
+
+const LandingInfo = (props: LandingInfoProps) => {
+  return (
+    <div className={props.containerStyle}>
+      <div>{props.leftText}</div>
+      <div>{props.rightText}</div>
+    </div>
+  );
+};
+export default LandingInfo;
