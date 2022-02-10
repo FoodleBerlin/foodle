8a340ba67fae631f8737bee0260f62e6a9479785
Alex <alex.alber.aa@gmail.com>
2022-02-10 10:52:48 +0100

    feat: Added Navbar

---
+++ components/Layout/Navbar.tsx
@@ -0,0 +1,38 @@
+import { NextComponentType, NextPage } from "next";
+import Link from "next/link";
+import Image from "next/image";
+import React from "react";
+import styles from "./Navbar.module.scss";
+import Tab from "./Tab";
+import Search from "./Search";
+import { BurgerMenu } from "./BurgerMenu";
+
+const Navbar: NextComponentType = () => {
+  const [email, setEmail] = React.useState<string>("");
+  const handleSubmit = () => {
+    console.log(email);
+  };
+  return (
+    <nav className={styles["navbar"]}>
+      <div className={styles["navbar__logo"]}>
+        <Link href="/">
+          <a>
+            <Image
+              src="/foodle_logo.svg"
+              width={70}
+              height={45}
+              alt="Foodle Logo"
+            />
+          </a>
+        </Link>
+      </div>
+      <div className={styles["navbar__menu"]}>
+        <Tab href="/" title="EN/DE" />
+        <Tab href="/" title="How It Works" />
+        <Tab href="/" title="List Your Kitchen" />
+        <Tab href="/" title="Contact" />
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
