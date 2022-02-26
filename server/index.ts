7827068f8e8e1fb5ff93c822f3252cdaca2de5ef
eemmiillyy <emily.morgan@code.berlin>
2022-02-26 08:20:20 +0100

    fix overwrite

--- server/index.ts
+++ server/index.ts
@@ -1,22 +1,21 @@
-import { ApolloServer } from "apollo-server-express";
-import schema from "./schema";
-import { createContext } from "./context";
-import express from "express";
-import session from "express-session";
-import passport from "./passport";
-import forgeJWT from "../utils/forgeJWT";
-
+import { ApolloServer } from 'apollo-server-express';
+import schema from './schema';
+import { createContext } from './context';
+import express from 'express';
+import session from 'express-session';
+import passport from './passport';
+import forgeJWT from '../utils/forgeJWT';
 
 const app = express();
 app.use(passport.initialize());
 
-const SESSION_SECRET =  'keyboard cat';
-app.use(session(
-  { secret: SESSION_SECRET,
-    resave:false ,
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET ?? '',
+    resave: false,
     saveUninitialized: false,
-  }
-));
+  })
+);
 export const apollo: ApolloServer = new ApolloServer({
   // An executable GraphQL schema.
   schema,
@@ -30,28 +29,24 @@ const router = express.Router();
 export async function main() {
   await apollo.start();
   app.use(router);
-  apollo.applyMiddleware({app});
+  apollo.applyMiddleware({ app });
   app.listen({
-    port:5000,
-  })
+    port: 5000,
+  });
 }
 main();
 
-router.get('/api/auth',
-  passport.authenticate('google', { scope: ['profile', 'email'] }));
+router.get('/api/auth', passport.authenticate('google', { scope: ['profile', 'email'] }));
 
-router.get('/api/callback',(req: any, res:any, next) => { 
-  passport.authenticate('google', 
-    async (err: any, user:any) => {
-    console.log("ayeee" + user);
-      const token = await forgeJWT(user);
-        res.cookie('jwt', token, {
-          httpOnly: true,
-          secure: false, // true in prod,
-          sameSite: 'lax', // 'strict' in prod,
-        });
-        return res.redirect('http://localhost:3000');
-    
-  }) (req,res,next);
-})
-;
+router.get('/api/callback', (req: any, res: any, next) => {
+  passport.authenticate('google', async (err: any, user: any) => {
+    console.log('ayeee' + user);
+    const token = await forgeJWT(user);
+    res.cookie('jwt', token, {
+      httpOnly: true,
+      secure: false, // true in prod,
+      sameSite: 'lax', // 'strict' in prod,
+    });
+    return res.redirect('http://localhost:3000');
+  })(req, res, next);
+});
--- server/passport.ts
+++ server/passport.ts
@@ -1,68 +1,71 @@
-const GoogleStrategy = require('passport-google-oauth20');
-import passport from  'passport';
-import prisma from '../server/singletons/prisma'
+const GoogleStrategy = require("passport-google-oauth20");
+import passport from "passport";
+import prisma from "../server/singletons/prisma";
 
-
-passport.use(new GoogleStrategy({
-    clientID: "921604575160-muj1vijp8gc4apqrj9uca0eht01m74hp.apps.googleusercontent.com",
-    clientSecret: "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",
-    callbackURL: "http://localhost:5000/api/callback",
-    state: true,
-  },
-  async function(accessToken: unknown, refreshToken: unknown, profile: any, cb: any) {
-      try{
-        if (!accessToken || !profile) return cb('error', null);
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:5000/api/callback",
+      state: true,
+    },
+    async function (
+      accessToken: unknown,
+      refreshToken: unknown,
+      profile: any,
+      cb: any
+    ) {
+      try {
+        if (!accessToken || !profile) return cb("error", null);
         const user = await prisma.user.findUnique({
           where: {
-            email : profile?.emails[0].value,
-          }
-        })
+            email: profile?.emails[0].value,
+          },
+        });
         if (!user) {
           const res = await prisma.user.create({
             data: {
-            kind: "user",
-            handle: profile.emails[0].value,
-            fullName: profile.displayName,
-            email: profile.emails[0].value,
-            image: profile.picture,
-            zip: -1,
-            description: "I am from ...",
-            isVerified: false,
-            role: "tenant", 
-            bookings: {create: []},
-            rentingOut: {create : []},
-            superOwner: false,
-            passport: "passportString",
-            passportVerified: false,
-            license: "licenseString",
-            licenseVerified: false,
-            solvency:"solvencyString",
-            solvencyVerified: false,
-            }
-        })
-        return cb(null, res);
-      } else {
-        return cb(null, user);
+              kind: "user",
+              handle: profile.emails[0].value,
+              fullName: profile.displayName,
+              email: profile.emails[0].value,
+              image: profile.picture,
+              zip: -1,
+              description: "I am from ...",
+              isVerified: false,
+              role: "tenant",
+              bookings: { create: [] },
+              rentingOut: { create: [] },
+              superOwner: false,
+              passport: "passportString",
+              passportVerified: false,
+              license: "licenseString",
+              licenseVerified: false,
+              solvency: "solvencyString",
+              solvencyVerified: false,
+            },
+          });
+          return cb(null, res);
+        } else {
+          return cb(null, user);
+        }
+      } catch (e) {
+        return cb(e);
       }
-    } catch (e){
-      return cb(e);
     }
-  }
-));
-
-
+  )
+);
 
 passport.serializeUser((user, done) => {
-    process.nextTick(function () {
-    done(null, { id: 'testyes' });
+  process.nextTick(function () {
+    done(null, { id: "testyes" });
   });
 });
 passport.deserializeUser((id, done) => {
- process.nextTick(function () {
-    return done(null, { id: 'testyes' });
+  process.nextTick(function () {
+    return done(null, { id: "testyes" });
   });
 });
-
-
 
 export default passport;
