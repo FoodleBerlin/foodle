941fac2b0e5cff2b062ceb26ced4ea5606c486d2
tomADC443 <tom@tpl.dev>
2022-03-10 14:13:27 +0100

    style: added class names for styling

--- components/Layout/wizard/Step2.tsx
+++ components/Layout/wizard/Step2.tsx
@@ -8,62 +8,67 @@ export default function Step2() {
   return (
     <div>
       <h1>Landlord component flow 2</h1>
-      <h2 className='header-secondary' >How would you describe the space?</h2>
-      <div className={styles['step1__flexWrapper']}>
-      <textarea 
-        className= 'standard-form'
-        placeholder='This is what users will see as the description under the overview tab on the listing page.'
-        {...register('description')}
-        onChange={(c) =>
-          setValue('description', c.target.value, {
-            shouldTouch: true,
-            shouldDirty: true,
-            shouldValidate: true,
-          })
-        }
-      ></textarea>
-      </div>
-      {formState.errors.description && <span className={styles['error']}>{formState.errors.description.message}</span>}
-      <h2 className='header-secondary'>What features does your kitchen offer?</h2>
-      <input
-        {...register('features')}
-        onChange={(c) =>
-          setValue('features', c.target.value as FormData['features'], {
-            shouldTouch: true,
-            shouldDirty: true,
-            shouldValidate: true,
-          })
-        }
-      ></input>
-      {formState.errors.features && <span className={styles['error']}>{formState.errors.features.message}</span>}
-      <h2 className='header-secondary'>What’s the minimum stay?</h2>
-      <input
-        type="number"
-        {...register('stay.hours')}
-        onChange={(c) =>
-          setValue('stay.hours', parseInt(c.target.value), {
-            shouldTouch: true,
-            shouldDirty: true,
-            shouldValidate: true,
-          })
-        }
-      ></input>
-      <p>Hours per week</p>
-      {formState.errors.stay?.hours && <span className={styles['error']}>{formState.errors.stay?.hours.message}</span>}
-      <input
-        type="number"
-        {...register('stay.weeks')}
-        onChange={(c) =>
-          setValue('stay.weeks', parseInt(c.target.value), {
-            shouldTouch: true,
-            shouldDirty: true,
-            shouldValidate: true,
-          })
-        }
-      ></input>
-      <p>Recurring weeks</p>
-      {formState.errors.stay?.weeks && <span className={styles['error']}>{formState.errors.stay?.weeks.message}</span>}
-      <button onClick={() => wizardContext.previousStep(2)}>previous</button>
+      <div className={styles['step2']}>
+        <h2 className='header-secondary' >How would you describe the space?</h2>
+        <p className='body-text'>This is what users will see as the description under the overview tab on the listing page.</p>
+        <div className={styles['step1__flexWrapper']}>
+          <textarea
+            className={styles['step2__textArea'] + ' standard-form'}
+            {...register('description')}
+            onChange={(c) =>
+              setValue('description', c.target.value, {
+                shouldTouch: true,
+                shouldDirty: true,
+                shouldValidate: true,
+              })
+            }
+          ></textarea>
+        </div>
+        {formState.errors.description && <span className={styles['error']}>{formState.errors.description.message}</span>}
+        <h2 className='header-secondary'>What features does your kitchen offer?</h2>
+        <input
+          {...register('features')}
+          onChange={(c) =>
+            setValue('features', c.target.value as FormData['features'], {
+              shouldTouch: true,
+              shouldDirty: true,
+              shouldValidate: true,
+            })
+          }
+        ></input>
+        {formState.errors.features && <span className={styles['error']}>{formState.errors.features.message}</span>}
+        <h2 className='header-secondary'>What’s the minimum stay?</h2>
+        <input
+          placeholder='0'
+          type="number"
+          {...register('stay.hours')}
+          onChange={(c) =>
+            setValue('stay.hours', parseInt(c.target.value), {
+              shouldTouch: true,
+              shouldDirty: true,
+              shouldValidate: true,
+            })
+          }
+        ></input>
+        <p>Hours per week</p>
+        {formState.errors.stay?.hours && <span className={styles['error']}>{formState.errors.stay?.hours.message}</span>}
+        <input
+          placeholder='0'
+          type="number"
+
+          {...register('stay.weeks')}
+          onChange={(c) =>
+            setValue('stay.weeks', parseInt(c.target.value), {
+              shouldTouch: true,
+              shouldDirty: true,
+              shouldValidate: true,
+            })
+          }
+        ></input>
+        <p>Recurring weeks</p>
+        {formState.errors.stay?.weeks && <span className={styles['error']}>{formState.errors.stay?.weeks.message}</span>}
+         </div>
+        <button onClick={() => wizardContext.previousStep(2)}>previous</button>
       <button
         disabled={formState.errors.description || formState.errors.features || formState.errors.stay ? true : false}
         onClick={() => {
