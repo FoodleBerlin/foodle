420fa40b0cdc3391d00bdd26b485addaf57a7287
tomADC443 <tom@tpl.dev>
2022-03-29 22:28:07 +0200

    style: changed styling to global sass

--- components/Book/TimeInput.tsx
+++ components/Book/TimeInput.tsx
@@ -3,28 +3,27 @@ interface TimeInputProps {
   handleChange: Function;
   shortest: string;
   short: string;
-  timeValue: {from:string, to:string};
+  timeValue: { from: string; to: string };
 }
 
 const TimeInput = (props: TimeInputProps) => {
   return (
     <>
-      <span className={styles['step3__timeInputWrapper'] + ' mb-one'}>
-
-        <input type="checkbox" checked id={'time-' + props.short} className="weekday" onChange={()=>{}}/> {/* empty "onChange" to suppress "missing onChange"-warning */}
+      <span className="weekdays__timeInputWrapper mb-one">
+        <input type="checkbox" checked id={'time-' + props.short} className="weekday" onChange={() => {}} />{' '}
+        {/* empty "onChange" to suppress "missing onChange"-warning */}
         <label className="bold" htmlFor={'time-' + props.short}>
           {props.shortest}
         </label>
         <input
-          onChange={(e)=>props.handleChange(props.short,"from",e.target.value)}
+          onChange={(e) => props.handleChange(props.short, 'from', e.target.value)}
           className="standard-form__inputTime"
           type="time"
           value={props.timeValue.from}
         />
-
         <label className="small-text bold"> to</label>
         <input
-          onChange={(e)=>props.handleChange(props.short,"to",e.target.value)}
+          onChange={(e) => props.handleChange(props.short, 'to', e.target.value)}
           className="standard-form__inputTime"
           type="time"
           value={props.timeValue.from}
