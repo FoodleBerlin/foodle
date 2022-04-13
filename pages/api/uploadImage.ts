14237c1a6e696284a4a6b13926309eb3629591cd
Alex <alex.alber.aa@gmail.com>
2022-04-13 18:34:19 +0200

    fix: Temporary security fix for uploading images- bucket accepts a max of 100 objects for now

--- pages/api/uploadImage.ts
+++ pages/api/uploadImage.ts
@@ -28,21 +28,27 @@ export const uploadResource= async (file:File | null, filename: string)=>{
 }
 
 export default async function handler(req: any, res: any, props: AuthenticatedProps) {
-
-  try { 
-    const post = await s3.createPresignedPost({
-      Bucket: process.env.AWS_S3_BUCKET_NAME,
-      Fields: {
-        key: req.query.file,
-      },
-      Expires: 60, // seconds
-      Conditions: [
-        ['content-length-range', 0, 5048576], // up to 1 MB
-      ],
-    })
-    
-    return res.status(200).json(post)
-  } catch (error) {
-    console.log(error)
-  }
+  s3.listObjectsV2({Bucket: 'foodle-bucket'}, (err, data)=> {
+    if (data.Contents!.length<100){
+      try { 
+        const post = s3.createPresignedPost({
+          Bucket: process.env.AWS_S3_BUCKET_NAME,
+          Fields: {
+            key: req.query.file,
+          },
+          Expires: 60, // seconds
+          Conditions: [
+            ['content-length-range', 0, 5048576], // up to 1 MB
+          ],
+        })
+        return res.status(200).json(post)
+      } catch (error) {
+        console.log(error)
+      }
+    } else {
+      return res.status(400).json({message: "Too many objects in bucket"})
+    }
+  })
+  
+  
 }
