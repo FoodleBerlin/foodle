fc6217a1ba47823e134fbd3d53cbb3f6c0ecb44a
Alex <alex.alber.aa@gmail.com>
2022-04-15 09:44:54 +0200

    feat: Made minor changes to profile page for testing new s3 integration

--- components/Profile/ProfileForm.tsx
+++ components/Profile/ProfileForm.tsx
@@ -1,8 +1,8 @@
 import { ChangeEvent, MutableRefObject, useEffect, useRef, useState } from 'react';
 import { FindUserQuery, FindUserResult, User, useUpdateUserMutation } from '../../codegen';
-import { UploaderImage } from '../Create/wizard/Step4';
+import { UploaderImg } from '../Create/wizard/Step4';
 import styles from '../../pages/account/Account.module.scss';
-import ProfileButton from './ProfileButton';
+import ProfileButton, { UploaderImage } from './ProfileButton';
 import { Token } from '../../utils/forgeJWT';
 import { urlSafeEncode } from '@aws-amplify/core';
 interface ProfileFormProps {
