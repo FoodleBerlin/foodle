d51196ca3b686bb91c8768d6d20c81794bc1ce38
tomADC443 <tom@tpl.dev>
2022-04-18 14:32:45 +0200

    test: added new tests and refactored old ones

---
+++ cypress/integration/shared/navbar.spec.js
@@ -0,0 +1,29 @@
+describe('Navbar', () => {
+  context('Visibility', () => {
+    beforeEach(() => {
+      cy.visit(Cypress.env('baseUrl'));
+    });
+    console.log('Routes:', Cypress.env('routes'));
+    const routes = Cypress.env('routes').filter((route) => {
+      return route.withNavbar === true;
+    });
+    console.log(routes.length);
+    routes.forEach((route) => {
+      console.log('Route with Navbar');
+      it(`is visible on ${route.path} route `, () => {
+        if (route.protected) {
+          cy.loginByGoogleApi();
+        }
+        cy.visit(Cypress.env('baseUrl') + route.path);
+        cy.get('*[class^="Navbar_navbar"]').should('be.visible');
+      });
+    });
+  });
+  context('Navigation', () => {
+    it('navigates to the login/signup page', () => {
+      cy.visit(Cypress.env('baseUrl'));
+      cy.get('button').contains('Login').click();
+      cy.contains('Log in or sign up');
+    });
+  });
+});
