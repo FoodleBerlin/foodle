b80865065d5385916bb839c44df60b00a90f1c37
tomADC443 <tom@tpl.dev>
2022-04-18 14:28:37 +0200

    feat: implemented google auth. with cypress

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,4 +1,4 @@
-/// <reference types="cypress" />
+// <reference types=cypress" />
 // ***********************************************************
 // This example plugins/index.js can be used to load plugins
 //
@@ -16,7 +16,16 @@
  * @type {Cypress.PluginConfig}
  */
 // eslint-disable-next-line no-unused-vars
-module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+
+require('dotenv').config()
+ const config =  (on, config) => {
+  // copy any needed variables from process.env to config.env
+  config.env.googleRefreshToken = process.env.CYPRESS_GOOGLE_REFRESH_TOKEN;
+  config.env.googleClientId = process.env.GOOGLE_CLIENT_ID;
+  config.env.googleClientSecret = process.env.GOOGLE_CLIENT_SECRET;
+  config.env.serverSecret = process.env.SERVER_SECRET;
+  console.log("Refresh Token: ",process.env.CYPRESS_GOOGLE_REFRESH_TOKEN);
+  // do not forget to return the changed config object!
+  return config
 }
+export default config
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,50 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+import jwt from 'jsonwebtoken';
+
+Cypress.Commands.add('loginByGoogleApi', () => {
+  cy.log('Logging in to Google');
+  cy.request({
+    method: 'POST',
+    url: 'https://www.googleapis.com/oauth2/v4/token',
+    body: {
+      grant_type: 'refresh_token',
+      client_id: Cypress.env('googleClientId'),
+      client_secret: Cypress.env('googleClientSecret'),
+      refresh_token: Cypress.env('googleRefreshToken'),
+    },
+  }).then(({ body }) => {
+    const { access_token, id_token } = body;
+
+    cy.request({
+      method: 'GET',
+      url: 'https://www.googleapis.com/oauth2/v3/userinfo',
+      headers: { Authorization: `Bearer ${access_token}` },
+    }).then(({ body }) => {
+      cy.log(body);
+
+      const tokenRaw = {
+        user: {
+          id: body.sub,
+          fullName: body.given_name +" " +body.family_name,
+          email: body.email,
+          stripeId: "cus_Kza1oi2OTlvcpb",
+        },
+      };
+
+      const token = jwt.sign(tokenRaw, Cypress.env('serverSecret') ?? '', {
+        expiresIn: '1d',
+      });
+      cy.setCookie("jwt", token, {httpOnly:true, sameSite: "lax"})
+
+      //cy.visit('http://localhost:3000');
+    });
+  });
+});
+
+
+ 
+// Custom method to check for dynamic class names
+Cypress.Commands.add('hasClass', () => {});
