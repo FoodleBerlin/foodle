2b8ba7ac2d915daf7dbc8defac036ed07f848b58
Alex <alex.alber.aa@gmail.com>
2022-05-11 11:29:54 +0200

    fix: Remove print of SERVER_URL

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,8 +6,6 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 export const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
-  console.log('SERVER_URL:' + process.env.SERVER_URL + 'api/auth');
-
   return (
     <QueryClientProvider client={queryClient}>
       <Component {...pageProps} />
