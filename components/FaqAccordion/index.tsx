f7533a9838bc4d62bcf0cf5aa0ec3c8922978884
jungwooleecode <jung.lee@code.berlin>
2022-09-16 17:07:03 +0200

    feat: rename the class and type and div classnames

--- components/FaqAccordion/index.tsx
+++ components/FaqAccordion/index.tsx
@@ -1,31 +1,31 @@
 
-import React, {useState} from 'react';
 import Image from 'next/image';
+import React from 'react';
 import styles from './FaqAccordion.module.scss';
 
-export type AccordinProps= { 
-    title: string, 
-    content: string
+export type FaqAccordinProps = {
+  title: string,
+  content: string
 }
 
 
-const Accordion=(props: AccordinProps) => {
-    const [isActive, setIsActive] = React.useState<boolean>(false);
-   
-  
-    return (
-      <div  className="">
-        <div className={styles['main__onClick']} onClick={() => setIsActive(!isActive) }>
-          <div className={styles['main__onClick__title']}>{props.title}</div>
-          <div className="">{isActive ? <Image  alt={'xmark'} src={'/xmark.svg'} width={15} height={15} /> : <Image alt={'plus'} src={'/plus.svg'} width={15} height={15} />}</div>
-        </div>
-        {isActive && <div className={styles['main__content']}>{props.content}</div>}
+const FaqAccordion = (props: FaqAccordinProps) => {
+  const [isActive, setIsActive] = React.useState<boolean>(false);
 
-        <hr />
 
+  return (
+    <div>
+      <div className={styles['question']} onClick={() => setIsActive(!isActive)}>
+        <div className={styles['question__title']}>{props.title}</div>
+        <div className="">{isActive ? <Image alt={'xmark'} src={'/xmark.svg'} width={15} height={15} /> : <Image alt={'plus'} src={'/plus.svg'} width={15} height={15} />}</div>
       </div>
-    );
-  };
-  
-  export default Accordion;
+      {isActive && <div className={styles['question__content']}>{props.content}</div>}
+
+      <hr />
+
+    </div>
+  );
+};
+
+export default FaqAccordion;
 
