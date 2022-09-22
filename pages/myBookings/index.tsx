b7e21431dc7888477d872dc29a2b52d8567838cd
jungwooleecode <jung.lee@code.berlin>
2022-09-22 18:39:18 +0200

    feat: remove bookinglist and import booking card component

--- pages/myBookings/index.tsx
+++ pages/myBookings/index.tsx
@@ -5,6 +5,8 @@ import Navbar from '../../components/Layout/Navbar';
 import Sidebar from '../../components/Layout/Sidebar'
 import Head from 'next/head';
 import styles from './myBookings.module.scss';
+import BookingCard from '../../components/BookingCard/index';
+
 
 
 const myBookings: NextPage=()=>{
@@ -18,6 +20,7 @@ const myBookings: NextPage=()=>{
         duration: string;
         startDate: string;
         img: string;
+        status: string;
     };
 
     const bookingData: Booking[]=[
@@ -29,7 +32,8 @@ const myBookings: NextPage=()=>{
         availableDays: ['Monday','Tuesday'],
         duration: '3 Months',
         startDate: 'Feb 5',
-        img: '/kitchen-image-1.png'
+        img: '/kitchen-image-1.png',
+        status: 'REQUESTED'
     },
     {
         id: '3857976752',
@@ -39,8 +43,8 @@ const myBookings: NextPage=()=>{
         availableDays: ['Monday','Thursday'],
         duration: '1 Month',
         startDate: 'Feb 5',
-      
-        img: '/kitchen-image-2.png'
+        img: '/kitchen-image-2.png',
+        status: 'CANCELED'
     },
     {
         id: '103832352',
@@ -50,8 +54,8 @@ const myBookings: NextPage=()=>{
         availableDays: ['Tuesday','Friday'],
         duration: '4 Months',
         startDate: 'Jan 5',
-    
-        img: '/kitchen-image-3.png'
+        img: '/kitchen-image-3.png',
+        status: 'REJECTED'
     },
     {
         id: '7493492948',
@@ -61,8 +65,8 @@ const myBookings: NextPage=()=>{
         availableDays: ['Saturday','Sunday'],
         duration: '3 Months',
         startDate: 'Jan 5',
-     
-        img: '/kitchen-image-4.png'
+        img: '/kitchen-image-4.png',
+        status: 'REJECTED'
     }
     ];
 
@@ -84,86 +88,14 @@ const myBookings: NextPage=()=>{
                 <Sidebar/>
 
                 <div className={styles['bookingList']}>
-                    <h1>My bookings</h1>
-                    <h2>A list of all bookings made, requested, rejected, cancelled and confirmed</h2>
+                    <h4>My bookings</h4>
+                    <h5>A list of all bookings made, requested, rejected, cancelled and confirmed</h5>
                    
-
                     <div className="">
-                        <h3>Requested</h3>
-                        
-                        <div className={styles['bookingList__info']}>
-                            <div className={styles['bookingList__info__box']}>
-                                <Image alt={'Kitchen Image'} src={bookingData[0].img} width={350} height={250} />
-                                <div className={styles['bookingList__info__box__text']}>
-                                    <h4>{bookingData[0].name} in {bookingData[0].area}</h4>
-                                    <h5>{bookingData[0].availableDays.join(", ")} <br /> {bookingData[0].startDate}-{bookingData[0].endDate} ({bookingData[0].duration})</h5>
-                                    <h6>Booking ref: <br />{bookingData[0].id}</h6>
-                                </div>
-                            </div>
-                            
-                            <button className={styles['bookingList__btn']}>DELETE</button>
-                        </div>
-
-                        <hr />
-                    </div>
-
-                    <div className="">
-                        <h3>Confirmed</h3>
-
-                        <p>No confirmed bookings yet.</p>
-
-                        <hr />
-                    </div>
-
-                    <div className="">
-                        <h3>Canceled</h3>
-
-                        <div className={styles['bookingList__info']}>
-                            <div className={styles['bookingList__info__box']}>
-                                <Image alt={'Kitchen Image'} src={bookingData[1].img} width={350} height={250} />
-                                <div className={styles['bookingList__info__box__text']}>
-                                    <h4>{bookingData[1].name} in {bookingData[1].area}</h4>
-                                    <h5>{bookingData[1].availableDays.join(", ")} <br /> {bookingData[1].startDate}-{bookingData[1].endDate} ({bookingData[1].duration})</h5>
-                                    <h6>Booking ref: <br />{bookingData[1].id}</h6>
-                                </div>
-                            </div>
-                            
-                            <button className={styles['bookingList__btn']}>DELETE</button>
-                        </div>
-
-                        <hr />
-                    </div>
-
-                    <div className="">
-                        <h3>Rejected</h3>
-
-                        <div className={styles['bookingList__info']}>
-                            <div className={styles['bookingList__info__box']}>
-                                <Image alt={'Kitchen Image'} src={bookingData[2].img} width={350} height={250} />
-                                <div className={styles['bookingList__info__box__text']}>
-                                    <h4>{bookingData[2].name} in {bookingData[2].area}</h4>
-                                    <h5>{bookingData[2].availableDays.join(", ")} <br /> {bookingData[2].startDate}-{bookingData[2].endDate} ({bookingData[2].duration})</h5>
-                                    <h6>Booking ref: <br />{bookingData[2].id}</h6>
-                                </div>
-                            </div>
-                            
-                            <button className={styles['bookingList__btn']}>DELETE</button>
-                        </div>
-
-                        <div className={styles['bookingList__info']}>
-                            <div className={styles['bookingList__info__box']}>
-                                <Image alt={'Kitchen Image'} src={bookingData[3].img} width={350} height={250} />
-                                <div className={styles['bookingList__info__box__text']}>
-                                    <h4>{bookingData[3].name} in {bookingData[3].area}</h4>
-                                    <h5>{bookingData[3].availableDays.join(", ")} <br /> {bookingData[3].startDate}-{bookingData[3].endDate} ({bookingData[3].duration})</h5>
-                                    <h6>Booking ref: <br />{bookingData[3].id}</h6>
-                                </div>
-                            </div>
-                            
-                            <button className={styles['bookingList__btn']}>DELETE</button>
-                        </div>
-
-                        <hr />
+                        {bookingData.map(({ id, area, name, endDate, availableDays, duration, startDate, img, status }) => (
+                            <BookingCard id={id} area={area} name={name} endDate={endDate} availableDays={availableDays} duration={duration} startDate={startDate} img={img} status={status} />
+                        ))}
+              
                     </div>
                 </div>
             </div>
