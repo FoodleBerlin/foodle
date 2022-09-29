7e979933726412357e7c7021c32c223ab8b9fbc6
jungwooleecode <jung.lee@code.berlin>
2022-09-29 18:50:31 +0200

    feat: add payment method and past payment blocks and paymentData

--- pages/payments/index.tsx
+++ pages/payments/index.tsx
@@ -3,10 +3,42 @@ import Head from 'next/head';
 import Navbar from '../../components/Layout/Navbar';
 import Sidebar from '../../components/Layout/Sidebar';
 import styles from './Payments.module.scss';
+import PastPayment from '../../components/PastPayment/index';
+import PaymentMethod from '../../components/PaymentMethod/index';
+
+export type Payment ={
+    date: string;
+    amount: number;
+    type: string;
+    status: string;
+    id: number;
+}
 
 const payments: NextPage= ()=>{
 
-
+    const paymentData: Payment[] =[
+        {
+            date: '01.22.2022',
+            amount: 900,
+            type: 'Reservation',
+            status: 'Paid',
+            id: 385792352
+        },
+        {
+            date: '01.22.2022',
+            amount: 900,
+            type: 'Reservation',
+            status: 'Paid',
+            id: 385792352
+        },
+        {
+            date: '01.22.2022',
+            amount: 900,
+            type: 'Reservation',
+            status: 'Paid',
+            id: 385792352
+        }
+    ]
 
 return(
 
@@ -20,7 +52,31 @@ return(
             <link rel="icon" href="/foodle_logo.svg" />
         </Head>
         <Navbar />
-        <Sidebar />
+        <div className={styles['payments']}>
+            <Sidebar />
+            <div className={styles['mypayments']}>
+                <h4 className="header-secondary">My payments</h4>
+                <h5 className="subtitle-text subtle-text">PAYMENTS PENDING, PAID, REFUNDED.</h5>
+                <div className={styles['paymentMethod']}>
+                    <h6 className='header-tertiary'>Payment methods</h6>
+                    <PaymentMethod />
+
+                </div>
+                <div className={styles['pastPayment']}>
+                    <h6 className='header-tertiary'>Past payments</h6>
+                    <div className={styles['namebox']}>
+                        <div>Date</div>
+                        <div>Amount</div>
+                        <div>Type</div>
+                        <div>Status</div>
+                        <div>Booking Ref</div>
+                    </div>
+                    <PastPayment payments={paymentData}/>
+
+                </div>
+            </div>
+        </div>
+        
 
     </div>
 
