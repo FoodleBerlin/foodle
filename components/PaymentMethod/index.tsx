f083dccf33763548f1f90ad29d4783e948f40cfd
jungwooleecode <jung.lee@code.berlin>
2022-10-04 15:29:00 +0200

    feat: update payment method

--- components/PaymentMethod/index.tsx
+++ components/PaymentMethod/index.tsx
@@ -1,10 +1,30 @@
 import styles from './PaymentMethod.module.scss'
+import {Method} from '../../pages/payments/index'
+import DefaultButton from '../DefaultButton/index'
 
-const PaymentMethod=()=>{
+export type PaymentMethodProps={
+    methods: Method[];
+}
+
+const PaymentMethod=(props: PaymentMethodProps)=>{
 
 return(
     <div className={styles['paymentMethod']}>
-       
+       {props.methods.length===0?(
+        <p>No payment method added yet.</p>
+       ):(
+        props.methods.map(({date,type,number,default})=>(
+            <div className="">
+                <div>{type} {number}</div>
+                <div>Expiry {date}</div>
+                <DefaultButton default={default}/>
+            </div>
+        ))
+       )
+       }
+
+
+
         <div className={styles['button']}><button>ADD PAYMENT METHOD</button></div>
         
     </div>
