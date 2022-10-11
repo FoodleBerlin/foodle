19c43acd8636c7ae99f17b2c2fbd4ebcdd037856
Alex <alex.alber.aa@gmail.com>
2022-10-11 15:32:31 +0200

    ref: remove dead code

--- client/components/Create/Preview/index.tsx
+++ client/components/Create/Preview/index.tsx
@@ -42,46 +42,6 @@ const Preview = (props: PreviewProps) => {
       return [...temp];
     });
   };
-  const [isHovering0, setIsHovered0] = useState(false);
-  const [isHovering1, setIsHovered1] = useState(false);
-  const [isHovering2, setIsHovered2] = useState(false);
-  const [isHovering3, setIsHovered3] = useState(false);
-  const [isHovering4, setIsHovered4] = useState(false);
-  const updateIdHover: (id: number, update: boolean) => void = (id: number, update: boolean) => {
-    switch (id - 1) {
-      case 0:
-        return setIsHovered0(update);
-      case 1:
-        return setIsHovered1(update);
-      case 2:
-        return setIsHovered2(update);
-      case 3:
-        return setIsHovered3(update);
-      case 4:
-        return setIsHovered4(update);
-    }
-  };
-  const isIdHovered = (id: number) => {
-    switch (id - 1) {
-      case 0:
-        return isHovering0;
-      case 1:
-        return isHovering1;
-      case 2:
-        return isHovering2;
-      case 3:
-        return isHovering3;
-      case 4:
-        return isHovering4;
-    }
-  };
-
-  const onMouseEnter = (event: React.MouseEvent<HTMLDivElement>) => {
-    updateIdHover(Number(event.currentTarget.id), true);
-  };
-  const onMouseLeave = (event: React.MouseEvent<HTMLDivElement>) => {
-    updateIdHover(Number(event.currentTarget.id), false);
-  };
 
   return (
     <>
@@ -94,17 +54,13 @@ const Preview = (props: PreviewProps) => {
                   style={'gallery__item--' + index}
                   image={image}
                   index={index}
-                  onMouseEnter={onMouseEnter}
-                  onMouseLeave={onMouseLeave}
                   handleOver={(e) => handleOver(e)}
                   handleDrag={(e) => handleDrag(e)}
                   handleDrop={(e) => handleDrop(e)}
-                  isIdHovered={() => isIdHovered(index)}
                   key={index}
                   deleteImage={(id) => props.deleteImage(id)}
                 />
               );
-              //   previewImage(image, React.createRef<HTMLDivElement>(), index)
             })}
         </div>
       </div>
