e56f08b9ab8b71934c220da058657f987304fedf
Flajt <matrimkurz@gmail.com>
2022-10-11 12:36:01 +0200

    Feat: Allow to set custom alert message (cMessage) via AlertContext

--- client/components/Alert/index.tsx
+++ client/components/Alert/index.tsx
@@ -4,7 +4,7 @@ import { useAlertContext } from "./AlertContext";
 
 
 type AlertProps = {
-    message: string;
+    message?: string | undefined;
     type: "error" | "success" | "info";
 }
 
@@ -13,7 +13,7 @@ type AlertProps = {
 const Alert = (props: AlertProps) => {
     const alertContext = useAlertContext();
     const crossStyle = props.type !== "info" ? styles["imageWrapper--filterWhite"] : "";
-    return (<div className={styles[props.type + "Alert"]} hidden={alertContext.isHidden}>{props.message}
+    return (<div className={styles[props.type + "Alert"]} hidden={alertContext.isHidden}>{props.message === undefined ? alertContext.cMessage ?? "No message" : props.message}
         <div className={styles["imageWrapper"] + " " + crossStyle}>
             <Image alt="Dismiss Dialog Button" src="/close.png" width={10} height={10} onClick={() => alertContext.shouldHide(true)} />
         </div>
