af9a907324d59a2323cedd36ecfe700363561c3a
jungwooleecode <jung.lee@code.berlin>
2022-10-12 16:35:00 +0200

    style: add styles to div classnames

--- components/Payment/index.tsx
+++ components/Payment/index.tsx
@@ -14,16 +14,16 @@ const Payment = (props: PaymentProps) => {
     return (
         <div className=''>
             {props.method.default ? (
-                <div>
+                <div className={styles['organizing']}>
                     <PaymentMethodInfo method={props.method}/>
                     <div className={styles['default']}>DEFAULT</div>
                 </div>
                 
             ) : (
-                <div>
+                <div className={styles['organizing']}>
                     <PaymentMethodInfo method={props.method}/>
-                    <div className="">
-                        <div className={styles['makeDefault']} onClick={()=> props.makeDefault(props.method)}>MAKE DEFAULT</div>
+                    <div className={styles['makeDefault']}>
+                        <div className={styles['makeDefault__btn']} onClick={()=> props.makeDefault(props.method)}>MAKE DEFAULT</div>
                         <Image className='' alt={'xmark'} src={'/xmark.svg'} width={18} height={18} onClick={()=>props.removeMethod(props.method)}/>
                     </div>
                 </div>
