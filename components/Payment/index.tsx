b7bbb38bdf48694b925243b6bbd8f2c2a5a0e374
jungwooleecode <jung.lee@code.berlin>
2022-10-12 15:28:00 +0200

    feat: add makeDefault function to onclick of make default text

--- components/Payment/index.tsx
+++ components/Payment/index.tsx
@@ -1,12 +1,15 @@
 import { Method } from '../../pages/payments/index';
 import Image from 'next/image';
 import styles from './Payment.module.scss';
+import {useState} from 'react';
 
 interface PaymentProps {
     method: Method;
+    makeDefault: (method: Method)=> void;
 }
 
 const Payment = (props: PaymentProps) => {
+
     return (
         <div className=''>
             {props.method.default ? (
@@ -25,7 +28,7 @@ const Payment = (props: PaymentProps) => {
                         <div className={styles['greytext']}>Expiry {props.method.date}</div>
                     </div>
                     <div className="">
-                        <div className={styles['makeDefault']}>MAKE DEFAULT</div>
+                        <div className={styles['makeDefault']} onClick={()=> props.makeDefault(props.method)}>MAKE DEFAULT</div>
                         <Image className='' alt={'xmark'} src={'/xmark.svg'} width={18} height={18} />
                     </div>
                 </div>
