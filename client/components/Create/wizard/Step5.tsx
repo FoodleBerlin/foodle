65322ecdfa908cfca67d25f2cf32d282a5462728
Flajt <matrimkurz@gmail.com>
2022-10-14 13:31:40 +0200

    Refactor: Remove comment

--- client/components/Create/wizard/Step5.tsx
+++ client/components/Create/wizard/Step5.tsx
@@ -2,6 +2,5 @@ import ListingsOverview from '../..//Listing/ListingOverview';
 import { useWizardContext } from './Wizard';
 export default function Step5() {
   const wiz = useWizardContext().getValues();
-  //console.log(JSON.stringify(wiz));
   return <ListingsOverview hideSidebar={true} owner="" handle={""} listingsData={wiz} />
 }
