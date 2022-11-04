436bf62843a8d33724ef74288cbfb8dca44b3599
conuko <constantinu93@gmail.com>
2022-11-04 17:38:51 +0100

    refactor: fix createListing test suite and more

--- server/tests/createListing.spec.ts
+++ server/tests/createListing.spec.ts
@@ -11,32 +11,77 @@ const descriptionToLong =
   'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nsdfsfsfsdfsdf';
 
 const query = `
-mutation CreateListing($size: Int!, $title: String!, $ownerHandle: String!, $street: String!, $streetNumber: String!, $zip: Int!, $city: String!, $description: String!, $hourlyPrice: Int!, $serviceFee: Int!, $rules: [String!]!, $deposit: Int!, $images: [String!]!, $partialSpace: Boolean!, $startDate: DateTime!, $endDate: DateTime!, $frequency: FrequencyEnum!, $facilities: [String!]!, $availableDays: [AvailableDay!]!) {
-  createListing(size: $size, title: $title, ownerHandle: $ownerHandle, street: $street, streetNumber: $streetNumber, zip: $zip, city: $city, description: $description, hourlyPrice: $hourlyPrice, serviceFee: $serviceFee, rules: $rules, deposit: $deposit, images: $images, partialSpace: $partialSpace, startDate: $startDate, endDate: $endDate, frequency: $frequency, facilities: $facilities, availableDays: $availableDays) {
-     Property {
+mutation CreateListing(
+  $size: Int!
+  $title: String!
+  $ownerHandle: String!
+  $street: String!
+  $streetNumber: String!
+  $zip: Int!
+  $city: String!
+  $description: String!
+  $hourlyPrice: Int!
+  $serviceFee: Int!
+  $rules: [String!]!
+  $deposit: Int!
+  $images: [String!]!
+  $partialSpace: Boolean!
+  $startDate: DateTime!
+  $endDate: DateTime!
+  $frequency: FrequencyEnum!
+  $facilities: [String!]!
+  $availableDays: [AvailableDay!]!
+  $pickup: Boolean
+) {
+  createListing(
+    size: $size
+    title: $title
+    ownerHandle: $ownerHandle
+    street: $street
+    streetNumber: $streetNumber
+    zip: $zip
+    city: $city
+    description: $description
+    hourlyPrice: $hourlyPrice
+    serviceFee: $serviceFee
+    rules: $rules
+    deposit: $deposit
+    images: $images
+    partialSpace: $partialSpace
+    startDate: $startDate
+    endDate: $endDate
+    frequency: $frequency
+    facilities: $facilities
+    availableDays: $availableDays
+    pickup: $pickup
+  ) {
+    Property {
+      kind
+      rules
+      handle
+      daySlots {
+        startTime
+        endTime
+      }
       title
       size
       owner {
-        id
-        fullName
-        email
-      }
-      bookings {
-        id
+        dob
       }
       street
       streetNumber
       zip
       city
       description
-      pickup
-      deposit
       images
       partialSpace
+      deposit
+      pickup
       isVerified
       hourlyPrice
       serviceFee
-      rules
+      facilities
+      frequency
     }
     ClientErrorUserNotExists {
       message
@@ -61,24 +106,26 @@ mutation CreateListing($size: Int!, $title: String!, $ownerHandle: String!, $str
 */
 
 const stdVars = {
-  ownerHandle: 'user4',
-  size: 0,
-  ownerId: '1',
-  street: 'FoodleStreet',
-  streetNumber: 0,
-  zip: 0,
-  city: 'Germany',
-  title: 'titlee',
-  description: '',
-  rules: [],
-  startDate: '2022-03-14T16:02:51.063Z',
-  endDate: '2022-05-14T16:02:51.063Z',
-  frequency: 'WEEKLY',
-  hourlyPrice: 0,
-  deposit: 0,
-  images: [''],
-  serviceFee: 0,
+  zip: 1355,
+  size: 123,
+  street: 'sample-street',
+  streetNumber: '23',
+  city: 'Berlin',
+  description: 'user1-listing',
+  serviceFee: 5,
+  rules: ['rule-1', 'rule-2'],
+  title: 'prop title',
+  hourlyPrice: 12,
+  deposit: 2,
+  images: ['test-image-url'],
   partialSpace: false,
+  ownerHandle: 'user4',
+  ownerId: '1',
+  handle: 'prop1',
+  frequency: 'WEEKLY',
+  facilities: ['Unfurnished'],
+  startDate: '2022-06-27T08:00:00.003Z',
+  endDate: '2022-06-30T08:00:00.003Z',
   availableDays: [
     {
       startTime: '2022-01-07T09:00:00.000Z',
@@ -90,7 +137,7 @@ const stdVars = {
 describe(' Property', () => {
   it('can create a listing, frequency weekly', async () => {
     const vars = { ...stdVars };
-    vars.city = 'Germany2';
+    vars.frequency = 'WEEKLY';
     const res = await server.executeOperation({
       query,
       variables: {
