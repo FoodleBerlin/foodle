daf64ee4ca0abb0dbd36a227a80ec33c59cef4b4
Flajt <matrimkurz@gmail.com>
2022-11-18 11:43:41 +0100

    Fix: Add more errror catching for image uploading

--- client/pages/api/uploadImage.ts
+++ client/pages/api/uploadImage.ts
@@ -22,28 +22,33 @@ export const uploadResource = async (file: File | null, filename: string) => {
 }
 
 export default async function handler(req: any, res: any, props: AuthenticatedProps) {
-  s3.listObjectsV2({ Bucket: 'foodle-bucket' }, async (err, data) => {
-    if (data.Contents!.length < 100) {
-      try {
-        const post = s3.createPresignedPost({
-          Bucket: process.env.NEXT_PUBLIC_AWS_S3_BUCKET_NAME,
-          Fields: {
-            key: req.query.file,
-          },
-          Expires: 60, // seconds
-          Conditions: [
-            ['content-length-range', 0, 5048576], // up to 1 MB
-          ],
-        })
-        return res.status(200).json(post)
-      } catch (error) {
-        res.status(500).json({ error: error })
+  try {
+    s3.listObjectsV2({ Bucket: 'foodle-bucket' }, async (err, data) => {
+      if (data.Contents!.length < 100) {
+        try {
+          const post = s3.createPresignedPost({
+            Bucket: process.env.NEXT_PUBLIC_AWS_S3_BUCKET_NAME,
+            Fields: {
+              key: req.query.file,
+            },
+            Expires: 60, // seconds
+            Conditions: [
+              ['content-length-range', 0, 5048576], // up to 1 MB
+            ],
+          })
+          return res.status(200).json(post)
+        } catch (error) {
+          res.status(500).json({ error: error })
+        }
+      } else {
+        return res.status(400).json({ message: "Too many objects in bucket" })
       }
-    } else {
-      return res.status(400).json({ message: "Too many objects in bucket" })
-    }
-    if (err) {
-      res.status(500).json({ awsError: err })
-    }
-  })
+      if (err) {
+        return res.status(500).json({ awsError: err })
+      }
+    })
+  }
+  catch (error) {
+    return res.status(500).json({ error2: error })
+  }
 }
